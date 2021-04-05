8ceaca8e9bbc0eee23597298040e68da4496a943
Lucas Costa <62567333+Lucasmirandac@users.noreply.github.com>
2021-04-05 13:24:45 -0300

    Atualização de projeto
    commit parcial de projeto

---
+++ controllers/atendimentos.js
@@ -0,0 +1,11 @@
+
+module.exports = app =>{
+    app.get('/atendimentos', (req,res) => {
+        res.send("Você está na rota de atendimentos e esta realizando um get")
+    })
+
+    app.post('/atendimentos', (req,res) => {
+        console.log(req.body)
+        res.send('Você está na rota de atendimentos e está realizando um post')
+    })
+}
